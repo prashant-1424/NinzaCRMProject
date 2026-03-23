40a64fe196ff30565fea2980df37d0d3a2eef7b6
Prashant <prashantsingh1424.ps@gmail.com>
2026-03-23 17:27:15 +0530

    commit

---
+++ Dummy/src/test/java/Demo/Program1.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		System.out.println("Hey");
+		System.out.println("Hello");
+		
+	}
+
+}
