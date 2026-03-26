714674bcbed23dad3208342e06f91a5995c6411f
Prashant <prashantsingh1424.ps@gmail.com>
2026-03-26 14:35:34 +0530

    Merge branch 'master' of https://github.com/prashant-1424/NinzaCRMProject.git

--- Dummy/src/test/java/Demo/Junior2.java
+++ Dummy/src/test/java/Demo/Junior2.java
@@ -1,7 +1,7 @@
 package Demo;
 
 public class Junior2 {
- public static void main(String[] args) {
-	System.out.println("Good evening everyone, I am Junior 2");
-}
+	public static void main(String[] args) {
+		System.out.println("Good evening everyone, I am Junior 2");
+	}
 }
---
+++ Dummy/src/test/java/Demo/PollSCM2.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class PollSCM2 {
+
+	public static void main(String[] args) {
+		System.out.println("I'm junior2, checking poll scm");
+	}
+
+}
